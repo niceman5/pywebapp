06ff8c185683fbd0409dffb92504c556e30d01cf
niceman555 <niceman555@gmail.com>
2020-06-08 23:31:35 +0900

    mini twit 완료

--- pyapp/minitwit/minitwit.py
+++ pyapp/minitwit/minitwit.py
@@ -9,7 +9,8 @@ from flask import Flask, request, session, url_for, redirect, render_template, a
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # configuration
-DATABASE = 'minitwit.db'
+# DATABASE = 'minitwit.db'        #경로표시하면 위치 지정 가능
+DATABASE = 'C:\\00.Dev\\Study\\pywebapp\\pyapp\\minitwit\\minitwit2.db'
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -119,7 +120,7 @@ def user_timeline(username):
         order by message.pub_date desc limit ?
     ''', [profile_user['user_id'], PER_PAGE], flolowed=flolowed, profile_user=profile_user))
 
-@app.route('<username>/follow')
+@app.route('/<username>/follow')
 def follow_user(username):
     #현재 사용자를 지정된 사용자의 팔로워로 추가
     if not g.user:
@@ -127,7 +128,7 @@ def follow_user(username):
     
     whom_id = get_user_id(username)
     
-    if whom_id is None
+    if whom_id is None:
         abort(404)
     
     g.db.execute('insert into follower (who_id, whom_id) values (?, ?)', [session['user_id'], whom_id])
@@ -143,10 +144,80 @@ def unfollow_user(username):
     
     whom_id = get_user_id(username)
     
-    if whom_id is None
+    if whom_id is None:
         abort(404)
     g.db.execute('delete from follewer who_id=? and whom_id = ?', [session['user_id'], whom_id])
     g.db.commit()
     flash('You are no longer following "%s"' % username)
     return redirect(url_for('user_timeline', username=username))
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    # 사용자에게 메시지 등록
+    if 'user_id' not in session:
+        abort(404)    
+    
+    if request.form['text']:
+        g.db.execute('''
+            insert into message(author_id, text, pub_date)
+            values(?,?,?) ''', (session['user_id'], request.form['text'], int(time.time())))
+        g.db.commit()
+        flash('your message was recorded')
+    
+    return redirect(url_for('timeline'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    #login user
+    if g.user:
+        return redirect(url_for('timeline'))    
+    error = None
+    if request.method=='POST':
+        user = query_db('''select * from user where username=?''', [request.form['username']], one=True)
+        if user is None:
+            error = 'Invalid username'
+        elif not check_password_hash(user['pw_hash'], request.form['password']):
+            error = 'Invalid password'
+        else:
+            flash('you were logged in')
+            session['user_id'] = user['user_id']
+            return redirect(url_for('timeline'))
+    return render_template('login.html', error=error)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    # 사용자 등록
+    if g.user:
+        return redirect(url_for('timeline'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter username'
+        elif not request.form['email'] or '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        elif get_user_id(request.form['username']) is not None:
+            error = 'The username is already taken'
+        else:
+            g.db.execute('''
+                insert into user(username, email, pw_hash) values(?,?,?)
+            ''', [request.form['username'], request.form['email'],generate_password_hash(request.form['password'])])
+            g.db.commit()
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
+
+@app.route('/logout')
+def logout():
+    flash('you were logged out')
+    session.pop('user_id', None)
+    return render_template(url_for('public_timeline'))
+
+app.jinja_env.filters['datetimeformat'] = format_datetime
+app.jinja_env.filters['gravatar'] = gravatar_url
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
     
