0dfceb80a58754625b2b2ffe4f7d7dfd83ffc2eb
niceman555 <niceman555@gmail.com>
2020-07-13 23:12:36 +0900

    session

--- pyapp/photolog/photolog/photolog_blueprint.py
+++ pyapp/photolog/photolog/photolog_blueprint.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import flask import Blueprint
-from photolog.photolog_logger import log
+from photolog.photolog_logger import Log
 
 photolog = Blueprint('photolog', __name__,
                     template_folder='../templates',
                     static_folder='../static')
-log.info('static folder: %s' % photolog.static_folder)
-log.info('template folder: %s' % photolog.template_folder)
+Log.info('static folder: %s' % photolog.static_folder)
+Log.info('template folder: %s' % photolog.template_folder)
                     
